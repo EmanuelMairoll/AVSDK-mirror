89baff89886b07a2c5e2a79ea3d4b17878d91e81
Emanuel Mairoll <emanuel.mairoll@authenticvision.com>
2024-03-08 11:56:21 +0100

    Update to version 8.2.2

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "AuthenticVisionSDK",
-            targets: ["AuthenticVisionSDK"]),
+            targets: ["AuthenticVisionSDK"])
     ],
     targets: [
         .binaryTarget(
